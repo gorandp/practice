32a0f549a107d4673a3031f818b6ded6b7fdd35e
gorandp <gorandp@outlook.com>
2020-01-12 17:07:17 -0300

    py casting learn

---
+++ Python/06 - Casting.py
@@ -0,0 +1,31 @@
+#integer casting
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+print(x)
+print(y)
+print(z)
+#print(type(x))
+#print(type(y))
+#print(type(z))
+print()
+
+#float casting
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+#w = float("4.2a") # ValueError: could not convert string to float: '4.2a'
+print(x)
+print(y)
+print(z)
+print(w)
+print()
+
+#string casting
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0' 
+print(x)
+print(y)
+print(z)
